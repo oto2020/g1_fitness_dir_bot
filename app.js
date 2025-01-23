d097cde56bccbadfb7406081afe49e660978914b
igo4ek <wawka2002@gmail.com>
2025-01-23 21:06:55 +0300

    расширенные изменения

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ bot.setMyCommands([
 
 // Временное хранилище для шагов регистрации
 const userSteps = {};
+const userMode = []
 
 // Проверка наличия пользователя в базе данных
 async function checkUser(chatId) {
@@ -39,6 +40,9 @@ async function checkUser(chatId) {
 bot.onText(/\/profile(.+)/, async (msg, match) => {
     const chatId = msg.chat.id;
     const telegramID = match[1]; // Получаем никнейм из команды
+    
+    userMode[chatId] = 'oneField';
+    
 
     // Проверяем, если пользователь отправил команду с никнеймом
     if (!telegramID) {
@@ -69,6 +73,7 @@ bot.onText(/\/profile(.+)/, async (msg, match) => {
 
 bot.onText(/\/name(.*)/, async (msg, match) => {
     const chatId = msg.chat.id;
+    userMode[chatId] = 'oneField';
     const telegramID = match[1].trim(); // Получаем telegramID из команды
 
     // Проверяем, если пользователь отправил команду с telegramID
@@ -126,8 +131,74 @@ bot.onText(/\/name(.*)/, async (msg, match) => {
     bot.on('message', nameHandler);
 });
 
+bot.onText(/\/role(.*)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    userMode[chatId] = 'updateRole';
+    const telegramID = match[1].trim(); // Получаем telegramID из команды
+
+    // Проверяем, если пользователь отправил команду с telegramID
+    if (!telegramID) {
+        bot.sendMessage(chatId, 'telegramID пользователя не указан. Пожалуйста, используйте формат: /updateRole telegramID');
+        return;
+    }
+
+    // Ищем пользователя по telegramID
+    const user = await prisma.user.findUnique({
+        where: { telegramID: parseInt(telegramID) },
+    });
+
+    if (!user) {
+        bot.sendMessage(chatId, `Пользователь с telegramID ${telegramID} не найден.`);
+        return;
+    }
+
+    // Запрашиваем новую роль у пользователя
+    bot.sendMessage(chatId, `Пожалуйста, введите новую роль для пользователя ${user.name}. Возможные варианты: пользователь, редактор, админ.`);
+
+    // Ожидаем получения текста с новой ролью
+    const roleHandler = (msg) => {
+        if (msg.chat.id !== chatId) return; // Игнорируем сообщения от других пользователей
+
+        const newRole = msg.text.trim().toLowerCase(); // Получаем новую роль из сообщения и переводим в нижний регистр
+
+        // Список допустимых ролей
+        const validRoles = ['пользователь', 'редактор', 'админ'];
+
+        // Проверяем, что роль является корректной
+        if (!validRoles.includes(newRole)) {
+            bot.sendMessage(chatId, 'Указана некорректная роль. Допустимые варианты: пользователь, редактор, админ. Попробуйте снова.');
+            return;
+        }
+
+        // Удаляем обработчик после первого срабатывания
+        bot.removeListener('message', roleHandler);
+
+        // Обновляем роль пользователя
+        prisma.user.update({
+            where: { telegramID: parseInt(telegramID) },
+            data: {
+                role: newRole, // Обновляем поле `role`
+            },
+        })
+            .then(() => {
+                bot.sendMessage(chatId, `Роль пользователя ${user.name} успешно обновлена на "${newRole}".`);
+                bot.sendMessage(process.env.GROUP_ID, `Обновлена роль пользователя ${user.name}:
+Новая роль: "${newRole}".`);
+            })
+            .catch((error) => {
+                console.error('Ошибка при обновлении роли:', error);
+                bot.sendMessage(chatId, 'Произошла ошибка при обновлении роли.');
+            });
+    };
+
+    // Добавляем обработчик для получения новой роли
+    bot.on('message', roleHandler);
+});
+
+
 bot.onText(/\/position(.*)/, async (msg, match) => {
     const chatId = msg.chat.id;
+    userMode[chatId] = 'oneField';
     const telegramID = match[1].trim(); // Получаем telegramID из команды
 
     // Проверяем, если пользователь отправил команду с telegramID
@@ -185,8 +256,38 @@ bot.onText(/\/position(.*)/, async (msg, match) => {
     bot.on('message', positionHandler);
 });
 
+bot.onText(/\/vpt_list(.*)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    userMode[chatId] = 'oneField';
+    const telegramID = match[1].trim(); // Получаем telegramID из команды
+
+    // Проверяем, если пользователь отправил команду с telegramID
+    if (!telegramID) {
+        bot.sendMessage(chatId, 'telegramID пользователя не указан. Пожалуйста, используйте формат: /position telegramID');
+        return;
+    }
+
+    // Ищем пользователя по telegramID
+    const user = await prisma.user.findUnique({
+        where: { telegramID: parseInt(telegramID) },
+    });
+
+    if (!user) {
+        bot.sendMessage(chatId, `Пользователь с telegramID ${telegramID} не найден.`);
+        return;
+    }
+
+    // Запрашиваем новую должность у пользователя
+    bot.sendMessage(chatId, `Пожалуйста, выберите проводимые ВПТ из списка ниже для ${user.name}.`);
+
+    // chatId это в каком чате нажата кнопка, telegramID это к какому пользователю относится
+    sendVptListInlineKeyboard(bot, chatId, telegramID);
+
+});
+
 bot.onText(/\/birthday(.*)/, async (msg, match) => {
     const chatId = msg.chat.id;
+    userMode[chatId] = 'oneField';
     const telegramID = match[1].trim(); // Получаем telegramID из команды
 
     // Проверяем, если пользователь отправил команду с telegramID
@@ -249,6 +350,7 @@ bot.onText(/\/birthday(.*)/, async (msg, match) => {
 
 bot.onText(/\/photo(.*)/, async (msg, match) => {
     const chatId = msg.chat.id;
+    userMode[chatId] = 'oneField';
     const telegramID = match[1]; // Получаем тг ид из команды
 
     // Проверяем, если пользователь отправил команду с никнеймом
@@ -322,10 +424,10 @@ bot.onText(/\/users/, async (msg) => {
         return;
     }
 
-    // Разбиваем список пользователей на группы по 10
+    // Разбиваем список пользователей на группы по 15
     const usersInGroups = [];
     while (users.length > 0) {
-        usersInGroups.push(users.splice(0, 10));
+        usersInGroups.push(users.splice(0, 15));
     }
 
 
@@ -335,7 +437,7 @@ bot.onText(/\/users/, async (msg) => {
 
         for (let i = 0; i < totalGroups; i++) {
             const group = groups[i];
-            const usersInfo = group.map((user) => generateUserInfo(user)).join('\n');
+            const usersInfo = group.map((user) => (`${user.name} @${user.nick}\nАнкета /profile${user.telegramID}\n`)).join('\n');
 
             // Отправляем сообщение с информацией о части группы
             const part = `${i + 1}/${totalGroups}`;
@@ -349,8 +451,6 @@ bot.onText(/\/users/, async (msg) => {
     // Отправляем пользователям информацию в группах
     sendUsersInfo(usersInGroups);
 });
-
-
 
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
@@ -380,8 +480,8 @@ bot.onText(/\/start/, async (msg) => {
 
     // Начлао регистрации
     // Сохраняем временные данные о пользователе
-    userSteps[chatId] = { step: 0, telegramID, nick };
-
+    userSteps[chatId] = { step: 0, telegramID, nick};
+    userMode[chatId] = 'userEdit';
     bot.sendMessage(chatId, 'Для начала, пожалуйста, поделитесь своим контактом. Нужно нажать кнопку "Поделиться контактом" в клавиатуре бота ниже.', {
         reply_markup: {
             keyboard: [
@@ -392,11 +492,9 @@ bot.onText(/\/start/, async (msg) => {
     });
 });
 
-
-
-
 bot.on('contact', async (msg) => {
     const chatId = msg.chat.id;
+    userMode[chatId] = 'userEdit';
     const contact = msg.contact;
 
     if (userSteps[chatId]?.step === 0) {
@@ -409,8 +507,6 @@ bot.on('contact', async (msg) => {
         bot.sendMessage(chatId, 'Спасибо за контакт! Теперь, пожалуйста, введите ваше ФИО:');
     }
 });
-
-
 
 // Обработка ввода должности
 bot.on('message', async (msg) => {
@@ -445,97 +541,144 @@ bot.on('message', async (msg) => {
         userSteps[chatId].birthday = birthday;
         userSteps[chatId].step = 4;
 
-        bot.sendMessage(chatId, 'Если вы тренер, выберите подразделения, в которых работаете и планируете проводить ВПТ.\nЕсли вы не тренер -- просто нажмите "Завершить выбор":', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: 'ТЗ', callback_data: 'ТЗ' }],
-                    [{ text: 'ГП', callback_data: 'ГП' }],
-                    [{ text: 'Аква', callback_data: 'Аква' }],
-                    [{ text: 'Завершить выбор', callback_data: 'done' }],
-                ],
-            },
-        });
+        sendVptListInlineKeyboard(bot, chatId, userSteps[chatId].telegramID);
     }
 });
+
+function sendVptListInlineKeyboard(bot, chatId, telegramID) {
+    bot.sendMessage(chatId, 'Если вы тренер, выберите подразделения, в которых работаете и планируете проводить ВПТ.\nЕсли вы не тренер -- просто нажмите "Завершить выбор":', {
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: 'ТЗ', callback_data: `vpt_list@tz@${telegramID}` }],
+                [{ text: 'ГП', callback_data: `vpt_list@gp@${telegramID}` }],
+                [{ text: 'Аква', callback_data: `vpt_list@aq@${telegramID}` }],
+                [{ text: 'Завершить выбор', callback_data: `vpt_list@done@${telegramID}` }],
+            ],
+        },
+    });
+}
 
 // Обработка выбора должностей
 bot.on('callback_query', async (query) => {
     const chatId = query.message.chat.id;
+    let user = await checkUser(chatId);
 
-    if (!userSteps[chatId] || userSteps[chatId].step !== 4) {
-        return;
-    }
-
-    const selection = query.data;
-
-    if (!userSteps[chatId].selections) {
-        userSteps[chatId].selections = [];
-    }
-
-    // Если выбор не сделан, добавляем его в список
-    if (selection !== 'done' && !userSteps[chatId].selections.includes(selection)) {
-        userSteps[chatId].selections.push(selection);
-    }
-
-    if (selection === 'done') {
-        const { name, position, selections, phoneNumber, telegramID, nick, birthday } = userSteps[chatId];
-        const finalPosition = `${position}${selections.map((sel) => `|${sel}`).join('')}`;
-        const timestamp = new Date();
-
-        const [day, month, year] = birthday.split('.'); // Разделяем дату
-        const isoBirthday = new Date(`${year}-${month}-${day}`).toISOString(); // Преобразуем в формат ISO
-
-        try {
-            await prisma.user.upsert({
-                where: { chatId },
-                update: {
-                    name,
-                    phoneNumber,
-                    position: finalPosition,
-                    nick,
-                    telegramID,
-                    timestamp,
-                    birthday: isoBirthday, // Добавляем дату рождения
-                },
-                create: {
-                    chatId,
-                    telegramID,
-                    nick,
-                    name,
-                    phoneNumber,
-                    position: finalPosition,
-                    birthday: isoBirthday, // Добавляем дату рождения
-                },
-            });
-
-            bot.sendMessage(chatId, `Ваши данные успешно сохранены!\nДоступные команды:\n/profile${telegramID}, чтобы увидеть свою анкету,\n/photo${telegramID}, чтобы установить фотографию,\n/user_edit, чтобы редактировать профиль.`);
-            bot.sendMessage(process.env.GROUP_ID, `Сохранена анкета пользователя ${name}:\n Просмотр: /profile${telegramID}`);
-
-        } catch (error) {
-            console.error('Ошибка при сохранении данных:', error);
-            bot.sendMessage(chatId, 'Произошла ошибка при сохранении данных.');
+    let queryTheme = query.data.split('@')[0];
+    let queryValue = query.data.split('@')[1];
+    let queryTelegramID = query.data.split('@')[2];
+    // перед @ тема нажатой кнопки, после @ значение нажатой кнопки
+    if (queryTheme === 'vpt_list') {
+        let selection = '';
+        if (queryValue === 'tz') {
+            selection = 'ТЗ';
+        }
+        if (queryValue === 'gp') {
+            selection = 'ГП';
+        }
+        if (queryValue === 'aq') {
+            selection = 'Аква';
         }
 
-        delete userSteps[chatId];
-    } else {
-        bot.sendMessage(chatId, `Вы выбрали: ${userSteps[chatId].selections.join(', ')}`);
+        if (!userSteps[chatId]) {
+            userSteps[chatId] = {};
+        }
+        if (!userSteps[chatId].selections) {
+            userSteps[chatId].selections = [];
+        }
+
+        // Если выбор не сделан, добавляем его в список
+        if (queryValue !== 'done' && !userSteps[chatId].selections.includes(selection)) {
+            userSteps[chatId].selections.push(selection);
+        }
+
+        if (queryValue === 'done') {
+            // кнопка "Завершить выбор" была нажата в режиме редактирования одного поля, а не завершения регистрации
+            if (userMode[chatId] === 'oneField') {
+                let vpt_list = userSteps[chatId].selections.join(', ');
+                // Обновляем проводимые ВПТ пользователя
+                prisma.user.update({
+                    where: { telegramID: parseInt(queryTelegramID) },
+                    data: {
+                        vpt_list: vpt_list, // Обновляем поле `vpt_list`
+                    },
+                })
+                    .then(async () => {
+
+                        let modifiedUser = await prisma.user.findUnique({
+                            where: { telegramID: parseInt(queryTelegramID) },
+                        });
+
+                        bot.sendMessage(chatId, `Проводимые ВПТ пользователя ${modifiedUser.name} успешно обновлены на "${vpt_list}".\nПросмотр: /profile${parseInt(queryTelegramID)}`);
+                        bot.sendMessage(process.env.GROUP_ID, `Обновлены проводимые ВПТ пользователя ${modifiedUser.name}:\nНовые проводимые ВПТ: "${vpt_list}"\nПросмотр: /profile${parseInt(queryTelegramID)}`);
+                    })
+                    .catch((error) => {
+                        console.error('Ошибка при обновлении проводимых ВПТ:', error);
+                        bot.sendMessage(chatId, 'Произошла ошибка при обновлении проводимых ВПТ.');
+                    });
+                    
+                delete userMode[chatId]; 
+                delete userSteps[chatId];
+            } 
+            if (userMode[chatId] === 'userEdit') {
+                const { name, position, selections, phoneNumber, telegramID, nick, birthday } = userSteps[chatId];
+                const vpt_list = `${selections.map((sel) => `${sel}`).join(', ')}`;
+                const timestamp = new Date();
+
+                const [day, month, year] = birthday.split('.'); // Разделяем дату
+                const isoBirthday = new Date(`${year}-${month}-${day}`).toISOString(); // Преобразуем в формат ISO
+
+                try {
+                    await prisma.user.upsert({
+                        where: { telegramID:queryTelegramID },
+                        update: {
+                            name,
+                            phoneNumber,
+                            position: position,
+                            vpt_list: vpt_list,
+                            nick,
+                            telegramID,
+                            timestamp,
+                            birthday: isoBirthday, // Добавляем дату рождения
+                        },
+                        create: {
+                            chatId,
+                            telegramID,
+                            nick,
+                            name,
+                            phoneNumber,
+                            position: position,
+                            vpt_list: vpt_list,
+                            birthday: isoBirthday, // Добавляем дату рождения
+                        },
+                    });
+
+                    bot.sendMessage(chatId, `Ваши данные успешно сохранены!\nДоступные команды:\n/profile${telegramID}, чтобы увидеть свою анкету,\n/photo${telegramID}, чтобы установить фотографию,\n/user_edit, чтобы редактировать профиль.`);
+                    bot.sendMessage(process.env.GROUP_ID, `Сохранена анкета пользователя ${name}:\n Просмотр: /profile${telegramID}`);
+
+                } catch (error) {
+                    console.error('Ошибка при сохранении данных:', error);
+                    bot.sendMessage(chatId, 'Произошла ошибка при сохранении данных.');
+                }
+
+                delete userSteps[chatId];
+            }
+
+        } else {
+            bot.sendMessage(chatId, `Вы выбрали: ${userSteps[chatId].selections.join(', ')}`);
+        }
     }
 });
 
 // Генерация информации о пользователе
 function generateUserInfo(user) {
-    return `Анкета: /profile${parseInt(user.telegramID)}\n` +
-        `${user.name} ${"@" + user.nick}\n` +
-        `- Телефон: ${user.phoneNumber}\n` +
-        `- Должность: ${user.position}\n` +
-        `- Дата рождения: ${user.birthday ? user.birthday.toLocaleDateString('ru-RU') : 'не указан'}\n` +
-        `- Фото: ${user.photo ? 'есть' : 'нет'}\n` +
-
-        ` Изменить пользователя:\n` +
-        `ФИО: /name${parseInt(user.telegramID)}\n` +
-        `Должность: /position${parseInt(user.telegramID)}\n` +
-        `Дата рождения: /birthday${parseInt(user.telegramID)}\n` +
-        `Новое фото: /photo${parseInt(user.telegramID)}\n`;
+    return `Анкета: /profile${parseInt(user.telegramID)}\n\n` +
+        `${user.name} ${"@" + user.nick}\n` + `Изменить /name${parseInt(user.telegramID)}\n\n` +
+        `- Телефон: \n${user.phoneNumber}\n\n` +
+        `- Должность: ${user.position}\nИзменить /position${parseInt(user.telegramID)}\n\n` +
+        `- Роль: ${user.role}\nИзменить /role${parseInt(user.telegramID)}\n\n` +
+        `- Проводимые ВПТ: ${user.vpt_list}\nИзменить /vpt_list${parseInt(user.telegramID)}\n\n` +
+        `- Дата рождения: ${user.birthday ? user.birthday.toLocaleDateString('ru-RU') : 'не указан'}\nИзменить /birthday${parseInt(user.telegramID)}\n\n` +
+        `- Фото: ${user.photo ? 'есть' : 'нет'}\nИзменить /photo${parseInt(user.telegramID)}\n-------------------------\n\n`;
 }
 
 
